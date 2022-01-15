7a68e4590501a45f8342be339ef859d317021a01
Marcos <exegeses@gmail.com>
2022-01-15 18:04:51 -0300

    Creación de tabla e inserción de datos

--- scripts/crearTabla.sql
+++ scripts/crearTabla.sql
@@ -7,6 +7,18 @@ USE introDB;
 -- crear una tabla
 CREATE TABLE billeteras
     (
-        id, nombre, precio, stock
-    )
+        id int primary key auto_increment not null,
+        nombre varchar(40) not null,
+        precio float(9,2) not null,
+        stock int not null,
+    );
+
+-- insertar datos en la tabla
+INSERT into billeteras
+    ( id, nombre, precio, stock )
+    VALUES
+        ( DEFAULT, 'Ledger Nano S', 16000, 30 ),
+        ( DEFAULT, 'Trezor One', 19500, 30 ),
+        ( DEFAULT, 'Trezor T', 78000, 20 ),
+        ( DEFAULT, 'Ledger Nano X', 56000, 20 );
 

e3b1d4e665fd7d39f98c741c228c81d3f1cae0d8
Marcos <exegeses@gmail.com>
2022-01-15 17:42:31 -0300

    script de comendos vistos clase 1

---
+++ scripts/crearTabla.sql
@@ -0,0 +1,12 @@
+-- Crear una base de datos
+CREATE DATABASE introDB;
+
+-- activar base de datos
+USE introDB;
+
+-- crear una tabla
+CREATE TABLE billeteras
+    (
+        id, nombre, precio, stock
+    )
+

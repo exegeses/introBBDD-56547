5bb40b9c885075f15704eb66d5e5b932801d6f93
Marcos <exegeses@gmail.com>
2022-01-22 15:20:18 -0300

    ver info de una tabla

---
+++ scripts/ver-info-tabla.sql
@@ -0,0 +1,9 @@
+-- activar una base de datos
+USE introDB;
+
+-- mostrar listado de tablas en una base
+SHOW TABLES;
+
+-- ver la estructura de una tabla ( campos y sus tipos de datos )
+-- DESCRIBE nombreTabla
+DESCRIBE billeteras;
